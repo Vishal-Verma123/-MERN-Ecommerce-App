9e1b7daa8f90aecf5483d8d4aaf8da39a42dec09
Vishal-Verma123 <49192030+Vishal-Verma123@users.noreply.github.com>
2023-06-11 16:53:40 +0530

    Frontend src files

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,15 +2,58 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import Menu from "./Pages/Menu";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import Newproduct from "./Pages/Newproduct";
+import Signup from "./Pages/Signup";
 import { store } from "./redux/index";
 import { Provider } from "react-redux";
+import Cart from "./Pages/Cart";
+import Success from "./Pages/Success";
+import Cancel from "./Pages/Cancel";
+import Forget_Password from "./Pages/Forget_Password";
+import MyProfile from "./Pages/MyProfile";
+import NotFound from "./Components/NotFound";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="*" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="menu" element={<Menu />} />
+      <Route path="menu/:filterby" element={<Menu />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="login" element={<Login />} />
+      <Route path="forget_password" element={<Forget_Password />} />
+      <Route path="myprofile" element={<MyProfile />} />
+      <Route path="newproduct" element={<Newproduct />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="success" element={<Success />} />
+      <Route path="cancel" element={<Cancel />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </Provider>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
